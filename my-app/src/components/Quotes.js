7498d058f3f233c771a91c90fbc2aac5c53b9a9f
Germano06 <57481143+Germano06@users.noreply.github.com>
2022-09-08 16:32:26 +0530

    author redirects to quote

--- my-app/src/components/Quotes.js
+++ my-app/src/components/Quotes.js
@@ -1,70 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export class Quotes extends Component {
-    constructor(){
-        super();
-        this.state={
-            quotes:[]
-        }
-    }
-    componentDidMount(){
-        let url = "https://quote-api-app.herokuapp.com/quote";
-        fetch(url)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-              this.setState({quotes:data})
-          });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+    };
+  }
 
-    handleChange(event) {
-        let author=event.target.value.toUpperCase();
-        let Quotes=document.getElementById('Quotes');
-        let Quote_box=Quotes.getElementsByTagName('div');
-        for(var i=0; i<Quote_box.length; i++)
-        {
-            let p=Quote_box[i].getElementsByTagName('p')[2];
-            if(p){
-                let textValue= p.textContent || p.innerHTML;
-                if(textValue.toUpperCase().indexOf(author) > -1){
-                    Quote_box[i].style.display="";
-                }else{
-                    Quote_box[i].style.display="none";
-                }
-            }
+  componentDidMount() {
+    let aut = sessionStorage.getItem("authName");
+    if (aut != null) {
+      document.getElementById("myInput").value = aut;
+      sessionStorage.removeItem("authName");
+    }
+    let url = "https://quote-api-app.herokuapp.com/quote";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ quotes: data });
+      });
+  }
+
+  handleChange(event) {
+    let author = event.target.value.toUpperCase();
+    let Quotes = document.getElementById("Quotes");
+    let Quote_box = Quotes.getElementsByTagName("div");
+    for (var i = 0; i < Quote_box.length; i++) {
+      let p = Quote_box[i].getElementsByTagName("p")[2];
+      if (p) {
+        let textValue = p.textContent || p.innerHTML;
+        if (textValue.toUpperCase().indexOf(author) > -1) {
+          Quote_box[i].style.display = "";
+        } else {
+          Quote_box[i].style.display = "none";
         }
       }
-  
-    render(){
-
-        return(
-       <>
-        <center><h1><Link to="/">Quotes</Link></h1></center>
-        <form className="d-flex" style={{margin:"30px"}}>
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"  id="myInput" onChange={this.handleChange}/>
-                    <button className="btn btn-outline-success" type="reset" >Reset</button>
-                  </form>
-       <div className='Quotes' id="Quotes">
-      {
-        
-       this.state.quotes.map(x=>{
-        return (         
-            <div id="Quote-box" className="Quote-box">
-            <p>{x.quote}</p>
-            <p className="likes">{x.likes}</p>
-            <p className='Author-name'>-{x.author}</p>
-            </div>
-            )
-       })
-      
     }
-    </div>
+  }
 
-        </>
-      )
+  render() {
+    return (
+      <>
+        <center>
+          <h1>
+            <Link to="/">Quotes</Link>
+          </h1>
+        </center>
+        <form className="d-flex" style={{ margin: "30px" }}>
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            id="myInput"
+            onChange={this.handleChange}
+          />
+          <button className="btn btn-outline-success" type="reset">
+            Reset
+          </button>
+        </form>
+        <div className="Quotes" id="Quotes">
+          {this.state.quotes.map((x) => {
+            return (
+              <div id="Quote-box" className="Quote-box">
+                <p>{x.quote}</p>
+                <p className="likes">{x.likes}</p>
+                <p className="Author-name">-{x.author}</p>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
   }
 }
 
-export default Quotes
+export default Quotes;
