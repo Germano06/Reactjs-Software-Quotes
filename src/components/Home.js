e1b5268ae9716f0f79ec93276c879aac29277db0
Germano06 <57481143+Germano06@users.noreply.github.com>
2022-09-15 18:36:21 +0530

    Random Quote done

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,18 +10,21 @@ export class Home extends Component {
   }
 
   componentDidMount() {
+    let rq = {};
     const getRandomQuote = (data, day) => {
-      if (sessionStorage.getItem("date") != null) {
-        console.log(day);
+      if (
+        localStorage.getItem("date") == null ||
+        sessionStorage.getItem("quote") == null
+      ) {
         const randomQuote = data[Math.floor(Math.random() * 84)];
         console.log(randomQuote);
-        sessionStorage.setItem("quote", randomQuote);
-        sessionStorage.setItem("date", day);
+        localStorage.setItem("date", day);
+        sessionStorage.setItem("quote", JSON.stringify(randomQuote));
+        console.log(day);
         return randomQuote;
       }
-      console.log(sessionStorage.getItem("quote"));
-      const randomQuote = sessionStorage.getItem("quote");
-      return randomQuote;
+      console.log(JSON.parse(sessionStorage.getItem("quote")));
+      return JSON.parse(sessionStorage.getItem("quote"));
     };
     let url = "https://quote-api-app.herokuapp.com/quote";
     fetch(url)
