f5b604019c35b8f6b019c0692fe3f441d806ba4b
sidesai2001 <sidesai2001@gmail.com>
2022-09-15 19:38:17 +0530

    Asssignment 2 complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,28 +10,30 @@ export class Home extends Component {
   }
 
   componentDidMount() {
-    let rq = {};
+  
     const getRandomQuote = (data, day) => {
       if (
-        localStorage.getItem("date") == null ||
-        sessionStorage.getItem("quote") == null
+        localStorage.getItem("date") == null || sessionStorage.getItem("quote") == null
       ) {
-        const randomQuote = data[Math.floor(Math.random() * 84)];
-        console.log(randomQuote);
+        let count = data.length
+        const randomQuote = data[Math.floor(Math.random() * count)];
+        
         localStorage.setItem("date", day);
         sessionStorage.setItem("quote", JSON.stringify(randomQuote));
-        console.log(day);
+
         return randomQuote;
       }
       console.log(JSON.parse(sessionStorage.getItem("quote")));
       return JSON.parse(sessionStorage.getItem("quote"));
     };
+
     let url = "https://quote-api-app.herokuapp.com/quote";
     fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
+      
         const now = new Date();
         const day = now.getDate();
         this.setState({ quotes: [getRandomQuote(data, day)] });
